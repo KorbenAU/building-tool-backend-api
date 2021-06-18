fde19cf76840b9681417be4633f3f24abca61219
Korben <gaoyuan.hello@gmail.com>
2021-06-18 15:18:36 +1000

    update session entity

--- Microservice.Database/Entities/Session.cs
+++ Microservice.Database/Entities/Session.cs
@@ -5,12 +5,9 @@ namespace Microservice.Database.Entities
     public class Session : BaseEntity
     {
         public int UserId { get; set; }
-        public int OrderId { get; set; }
         public string SessionIdentifier { get; set; }
         public string IpAddress { get; set; }
         public DateTime ExpiresAt { get; set; }
-        public string AccessKey { get; set; }
-        public DateTime? AccessKeyExpiry { get; set; }
 
         public virtual User User { get; set; }
     }
